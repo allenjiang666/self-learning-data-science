ad74d70d786390cbbf74eb42dd9a9b9ec19b8cec
Yipeng Jiang <yipengjiang0532@gmail.com>
2020-08-25 21:06:31 -0500

    add status bar

--- tkinter/image_viewer.py
+++ tkinter/image_viewer.py
@@ -26,7 +26,7 @@ root.title('Simple Image viewer')
 
 def show_image():
 	global my_label
-
+	global status_bar
 	# Have to make this variable global, otherwise it won't show
 	global img
 	# somehow .rotate can't not be used outside of the prenticis
@@ -34,6 +34,9 @@ def show_image():
 	my_label = Label(image=img)
 	my_label.grid(row=0, column=0, columnspan=4)
 
+	# add status bar. bd means border, relief means the lable is sunken, anchor means where you want to put the text
+	status_bar = Label(root, text= f'Image {str(img_num)} of {str(len(imgs))}', bd=1, relief=SUNKEN, anchor=E)
+	status_bar.grid(row=2, column=0, pady=10, columnspan=4, sticky=E+W)
 	# when reach first image, can't go further back
 	if img_num==0:
 		button_back['state']=DISABLED
@@ -92,10 +95,12 @@ button_back = Button(root, padx=30, pady =5, text='<<',command=backward)
 button_left = Button(root, padx=15, pady =5, text='rotate left',command=turn_left)
 button_right = Button(root, padx=15, pady =5, text='rotate right',command=turn_right)
 
+
 button_forw.grid(row=1, column=3, pady=10)
 button_back.grid(row=1, column=0, pady=10)
 button_left.grid(row=1, column=1, pady=10)
 button_right.grid(row=1, column=2, pady=10)
+
 
 show_image()
 
